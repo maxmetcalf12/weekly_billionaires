4a945fddc8ec73b575421ddb36df69209a64299f
maxmetcalf12 <maxmetcalf12@gmail.com>
2020-08-15 00:47:47 -0700

    style: updated style to match previous blog

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        mono: ['Anonymous Pro', ...defaultTheme.fontFamily.mono],
       },
     },
   },
