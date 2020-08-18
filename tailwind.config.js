1a1e8239c3afcc3df4f58a8e885923757095c578
maxmetcalf12 <maxmetcalf12@gmail.com>
2020-08-17 18:58:28 -0700

    style: updated a bunch of small style things

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
         mono: ['Anonymous Pro', ...defaultTheme.fontFamily.mono],
       },
       opacity: {
